547a2ff62bff687429c79c4ba662567697dc77bb
Rosemary <rdt13@ic.ac.uk>
2018-07-11 14:03:06 +0100

    added comments

--- test/TestDipoleDipole.jl
+++ test/TestDipoleDipole.jl
@@ -5,7 +5,18 @@ using Base.Test
 
 N, Edisorder, Jdisorder, modelJ, B, dipolestrength, r = init!(3, 0.0, 0.0, 0.001, 298, 20)
 
+"""
+Compares the results of a 3-site model when dipole-dipole interactions are/are
+not included.
 
+The dipole-dipole interaction field is calculated from the gradient of the
+potential, which scales as r^-6 for induced dipoles. The dipoles are first found
+from the field established by the charge distribution alone. The field is then
+updated to include the dipole-dipole effects and the dipoles are recalculated.
+
+Test will be successful if the effect of including these interactions results in
+a difference in the maximum density of less than 0.00001.
+"""
 function main()
 
     densities = zeros(2)
