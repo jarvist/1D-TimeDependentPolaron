206ecf9742e151e3242feaf64240f72989c6c5e6
Rosemary <rdt13@ic.ac.uk>
2018-07-11 15:08:24 +0100

    test to check if adiabatic propagation tends to a localised system when transfer integral is small

--- test/AdiabaticPropagationTest.jl
+++ test/AdiabaticPropagationTest.jl
@@ -7,9 +7,13 @@ push!(LOAD_PATH,"../src/")
 using TheDancer
 using Base.Test
 
-N, Edisorder, Jdisorder, modelJ, B, dipolestrength, r = init!(3, 0.0, 0.0, 0.001, 298, 20,1)
+N, Edisorder, Jdisorder, modelJ, B, dipolestrength, r = init!(3, 0.0, 0.0, 0.001, 298, 20)
 
-
+"""
+Test full adiabatic propagation for a simple 3-site system. With small transfer
+integral (J=0.001eV) the electron density should end up all at the central
+site.
+"""
 function main()
     S,E,H,psi,dipoles=prepare_model()
     density = abs.(psi.^2)
@@ -18,7 +22,7 @@ function main()
         S,H,psi,density,dipoles = AdiabaticPropagation(S,dipoles,E,false)
     end
 
-    ε = 1E-2
+    ε = 1E-6
     @test  1 ≈ density[2] atol = ε
 end
 
