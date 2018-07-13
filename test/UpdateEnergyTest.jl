96308e9cd44d745a989e736bc862e8aedb2cd54b
Rosemary <rdt13@ic.ac.uk>
2018-07-13 14:39:28 +0100

    Updated to test both value and symmetry of energy

--- test/UpdateEnergyTest.jl
+++ test/UpdateEnergyTest.jl
@@ -7,7 +7,7 @@ push!(LOAD_PATH,"../src/")
 using TheDancer
 using Base.Test
 
-N, Edisorder, Jdisorder, modelJ, B, dipolestrength, r = init!(3, 0.0, 0.0, 0.0, 298, 20)
+N, Edisorder, Jdisorder, modelJ, B, dipolestrength, r = init!(3, 0.0, 0.0, 0.0, 298, 8)
 
 """
 Test the on-site energy is correctly calculated as the potential energy felt by
@@ -15,7 +15,9 @@ a test charge on site i, in the presence of the electric fields established by
 all other sites.
 
 Testing in a symmetric 3-site environment, energy at the central position should
-be 0 as effects from dipoles on either side cancel each other out
+be 0 as effects from dipoles on either side cancel each other out.
+
+External Field should be set to zero.
 """
 
 function main()
@@ -24,7 +26,10 @@ function main()
     S = [0.0,0.0,0.0]
     S,H = SiteEnergyFromDipoles(dipoles,S,E)
 
+    S_check = 5/(4*r^2)
+
     ε = 1e-2
+    @test S_check ≈ S[1] atol = ε
     @test 0.0 ≈ S[2] atol = ε
 end
 
